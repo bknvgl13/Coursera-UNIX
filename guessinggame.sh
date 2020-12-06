93ac5813838ecc057bccc34e3c6fbe08bf1e3ea8
PollyAccord <62744114+PollyAccord@users.noreply.github.com>
2020-12-06 16:12:42 +0300

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
+
+#Algorithm: first, the number of files in the directory is calculated, then the expected number is entered. 
+#Then there is a comparison. The program ends when the user guesses the number correctly.
+
 function num_file() {
 num=$(-type f| wc -l)
 }
 
 function guess_file {
 	num = $num_file
-	echo "Enter your guess for number:"
+	echo "Enter your guess for the number of files in directory:"
 	read guess
-	if [[ $guess -eq num ]]
-	then
-	echo "Guessed!"
+	if [ "$guess" -eq "num" ]; then
+	echo "Guessed!" 
 		else
 		if [[ $guess -gt num ]]
 		then
 			echo "Too much, press Enter:"
 			guess_file
-		else
+		else if [[ $guess -lt num ]]
 			echo "Too less, press Enter:"
 			guess_file
 		fi
